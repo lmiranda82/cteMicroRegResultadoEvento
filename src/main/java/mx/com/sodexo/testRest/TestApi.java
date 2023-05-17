5c7414257ee2f3f6803b4f4ca2170b44eca05ee3
unknown <lr.miranda82@gmail.com>
2023-05-16 18:43:13 -0600

    Habilitacion de cliente

--- src/main/java/mx/com/sodexo/testRest/TestApi.java
+++ src/main/java/mx/com/sodexo/testRest/TestApi.java
@@ -1,15 +1,40 @@
 package mx.com.sodexo.testRest;
 
+import java.util.Arrays;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+import mx.com.sodexo.entidades.EventoRealizado;
+
 
 @RestController
 public class TestApi {
 	
+	RestTemplate restTemplate = new RestTemplate();
+	
 	@GetMapping(path = "/testA", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String probarGetEventosRealizador(){
-		return "Funciona ok";
+	public String probarGetEventoRealizado(){
+		
+		EventoRealizado evento1;
+		evento1 = this.restTemplate.getForObject("http://localhost:8080/eventosRealizados/{nEvento}", 
+				EventoRealizado.class, "1");
+		
+		return evento1.toString();
+	}
+	
+	@GetMapping(path = "/testB", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String probarGetEventosRealizados(){
+		
+		EventoRealizado[] eventos;
+		eventos = this.restTemplate.getForObject("http://localhost:8080/eventosRealizados", 
+				EventoRealizado[].class);
+		
+		return Arrays.asList(eventos).toString();
 	}
 
 }
